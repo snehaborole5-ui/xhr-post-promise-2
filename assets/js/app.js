f6da70210d38f4563043f37729a028eac5f39476
snehaborole5-ui <snehaborole5@gmail.com>
2026-06-19 12:01:32 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,266 @@
+const cl = console.log;
+const cardcontainer = document.getElementById('cardcontainer')
+const inputform = document.getElementById('inputform')
+const title = document.getElementById('title')
+const body = document.getElementById('body')
+const userId = document.getElementById('userId')
+const addpost = document.getElementById('addpost')
+const updatepost = document.getElementById('updatepost')
+const spinner = document.getElementById('spinner')
+
+let url = `https://crud-b-21-default-rtdb.asia-southeast1.firebasedatabase.app/`
+let base_url =`https://crud-b-21-default-rtdb.asia-southeast1.firebasedatabase.app/posts.json`
+
+let postArr =[]
+
+function snackbar(msg,icon){
+    swal.fire({
+        title : msg,
+        icon : icon,
+        timer : 2000
+    })
+}
+
+function makeApicall(method,url,body=null){
+   return new Promise((resolve,reject) =>{
+    let xhr = new XMLHttpRequest()
+    xhr.open(method,url)
+    xhr.send(body?JSON.stringify(body):null)
+        xhr.onload = function (){
+            if(xhr.status >= 200 && xhr.status <= 299){
+                let res = JSON.parse(xhr.response)
+                resolve(res)
+            }else{
+                let err = xhr.response
+                reject(err)
+            }
+        }
+   })
+}
+
+function fetchposts(){
+    spinner.classList.remove('d-none')
+    makeApicall('GET',base_url,null)
+        .then((res) =>{
+            cl(res)
+            for (const key in res) {
+                postArr.unshift({...res[key]})
+            }
+            crateCards(postArr)
+            $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+            })
+        })
+        .catch((err)=>{
+            snackbar(err,'error')
+        })
+        .finally(()=>{
+            spinner.classList.add('d-none')
+        })
+}
+
+fetchposts()
+
+function crateCards(arr){
+    let result =``
+    arr.forEach(ele=>{
+        result+=` <div class="col-md-4 my-4" id=${ele.id}>
+					<div class="card h-100">
+						<div class="card-header" data-toggle="tooltip" data-placement="top" title="${ele.title}" >
+							<h2>${ele.title}</h2>
+						</div>
+						<div class="card-body">
+							<p>${ele.body}</p>
+						</div>
+						<div class="card-footer d-flex justify-content-between">
+							<button class="btn btn-success btn-sm" onclick="onEdit('${ele.id}')">Edit</button>
+							<button class="btn btn-warning btn-sm" onclick="onRemove('${ele.id}')">Remove</button>
+
+						</div>
+					</div>
+				</div>`
+    })
+
+    cardcontainer.innerHTML = result
+}
+
+function onsubmithandl(ele){
+    spinner.classList.remove('d-none')
+
+    ele.preventDefault()
+
+    let newobj ={
+        title:title.value,
+        body : body.value,
+        userId : userId.value
+    }
+
+    makeApicall('POST',base_url,newobj)
+    .then((res)=>{
+        newobj.id = res.name
+
+        CreateNewCard(newobj)
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        })
+    })
+    .catch((err)=>{
+        snackbar(err,'error')
+    })
+    .finally(()=>{
+        spinner.classList.add('d-none')
+    })
+}
+
+function CreateNewCard(obj){
+    let div = document.createElement('div')
+    div.className = `col-md-4 my-4`
+    div.id = obj.id
+    div.innerHTML =`<div class="card h-100">
+						<div class="card-header" data-toggle="tooltip" data-placement="top" title="${obj.title}">
+							<h2>${obj.title}</h2>
+						</div>
+						<div class="card-body">
+							<p>${obj.body}</p>
+						</div>
+						<div class="card-footer d-flex justify-content-between">
+							<button class="btn btn-success btn-sm " onclick="onEdit('${obj.id}')">Edit</button>
+							<button class="btn btn-warning btn-sm " onclick="onRemove('${obj.id}')">Remove</button>
+
+						</div>
+					</div>`
+    
+    cardcontainer.prepend(div)
+
+    inputform.reset()
+
+    snackbar(`The New Post with id ${obj.id} is Added Successfully!!`,'success')
+    let putUrl = `${url}/posts/${obj.id}.json`
+    makeApicall('PUT',putUrl,obj)
+        .then((res)=>{
+            cl(res)
+        })
+        .catch((err)=>{
+            cl(err)
+        })
+}
+
+
+function onEdit(id){
+    spinner.classList.remove('d-none')
+    let editId = id
+    localStorage.setItem('EditId',editId)
+
+    let editUrl = `https://crud-b-21-default-rtdb.asia-southeast1.firebasedatabase.app/posts/${editId}.json`
+
+    makeApicall('GET',editUrl,null)
+        .then((res)=>{
+            title.value = res.title
+            userId.value = res.userId
+            body.value = res.body
+
+            addpost.classList.add('d-none')
+            updatepost.classList.remove('d-none')
+            inputform.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+             });
+
+
+        })
+        .catch((err)=>{
+            snackbar(err,'error')
+        })
+        .finally(()=>{
+            spinner.classList.add('d-none')
+        })
+
+}
+
+function onupdatehandl(){
+    spinner.classList.remove('d-none')
+
+    let updateId = localStorage.getItem('EditId')
+    let updateUrl = `${url}/posts/${updateId}.json`
+
+    let updatePost ={
+        title:title.value,
+        body : body.value,
+        userId : userId.value,
+        id : userId
+    }
+
+    makeApicall('PUT',updateUrl,updatePost)
+        .then((res)=>{
+            let div = document.getElementById(updateId)
+            let h2 = div.querySelector('.card-header h2')
+            h2.innerHTML = res.title
+
+            let p = div.querySelector('.card-body p')
+            p.innerHTML = res.body
+
+            addpost.classList.remove('d-none')
+            updatepost.classList.add('d-none')
+
+            inputform.reset()
+            snackbar(`The Post with Id ${updateId} is Updated Successfully!!`,'success')
+
+            
+            div.scrollIntoView({
+                behavior: 'smooth',
+                block: 'center'
+            });
+
+            div.classList.add('highlight');
+
+            setTimeout(() => {
+                div.classList.remove('highlight');
+            }, 4000);
+
+
+        })
+        .catch((err)=>{
+            snackbar(err,'error')
+        })
+        .finally(()=>{
+            spinner.classList.add('d-none')
+        })
+        
+}
+
+function onRemove(id){
+    Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            spinner.classList.remove('d-none')
+
+            let removeId = id
+            let removeURl = `${url}/posts/${removeId}.json`
+
+            makeApicall('DELETE',removeURl,null)
+                .then((res)=>{
+                    document.getElementById(removeId).remove
+                    snackbar(`The Post with Id ${removeId} is Removed Successfully!!`,'success')
+                })
+                .catch((err)=>{
+                    snackbar(err,'error')
+                })
+                .finally(()=>{
+                    spinner.classList.add('d-none')
+                })
+
+        }
+    });
+
+}
+
+
+inputform.addEventListener('submit',onsubmithandl)
+updatepost.addEventListener('click',onupdatehandl)
